7fc5b5a7a0f8ea6e95882c263cdd1c1b0218d5c3
Marcin Kunicki <masel.popowo@gmail.com>
2017-03-19 15:23:05 +0100

    Issue #2: Added angular material support

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,32 +1,29 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async } from "@angular/core/testing";
+import { MdToolbarModule } from "@angular/material";
+import { AppComponent } from "./app.component";
 
-import { AppComponent } from './app.component';
-
-describe('AppComponent', () => {
-  beforeEach(async(() => {
+describe('AppComponent', () =>{
+  beforeEach(async(() =>{
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        MdToolbarModule
+      ]
     }).compileComponents();
   }));
 
-  it('should create the app', async(() => {
+  it('should create the app', async(() =>{
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'etc works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('etc works!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
+  it('should render title in a h1 tag', async(() =>{
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('etc works!');
+    expect(compiled.querySelector('md-toolbar span').textContent).toContain('Ewicom Tank Chess');
   }));
 });
