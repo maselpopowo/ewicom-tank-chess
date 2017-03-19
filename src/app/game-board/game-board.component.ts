1584e576f0f09b1ca02391759fe7d11885d95bbe
Marcin Kunicki <masel.popowo@gmail.com>
2017-03-19 15:42:09 +0100

    Issue #3: Initialize etc-game-board component

---
+++ src/app/game-board/game-board.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: 'etc-game-board',
+  templateUrl: './game-board.component.html',
+  styleUrls: ['./game-board.component.scss']
+})
+export class GameBoardComponent implements OnInit {
+
+  constructor(){
+  }
+
+  ngOnInit(){
+  }
+
+}
