4bf49514dbe1c5f041251b3adc445ac589321326
Marcin Kunicki <masel.popowo@gmail.com>
2017-04-14 22:23:25 +0200

    Upgrade @angular/cli from 1.0.0-RC.1 to 1.0.0
    Upgrade @angular modules from 2.4.1 to 4.0.0
    Upgrade @angular/material from 1.0.0-beta.2 to 1.0.0-beta.3
    
    Issues
    Fix #28

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { HttpModule } from "@angular/http";
-import { MaterialModule } from "@angular/material";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MdButtonModule, MdCardModule, MdListModule, MdToolbarModule } from "@angular/material";
 import { AppComponent } from "./app.component";
 import { GameBoardComponent } from "./game-board/game-board.component";
 import { SquareComponent } from "./game-board/square.component";
@@ -26,7 +27,11 @@ import { TurnService } from "./turn.service";
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule
+    BrowserAnimationsModule,
+    MdCardModule,
+    MdToolbarModule,
+    MdButtonModule,
+    MdListModule
   ],
   providers: [
     BoardService,
